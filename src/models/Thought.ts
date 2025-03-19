748bacb507cc75736a8810f8ed4c26056e76a688
alyssadailey <alyssadailey28@gmail.com>
2025-03-19 14:48:13 -0400

    updated User model to fit requirements

---
+++ src/models/Thought.ts
@@ -0,0 +1,69 @@
+import { Schema, Types, model, type Document } from 'mongoose';
+
+interface IReaction extends Document {
+    reactionId: Schema.Types.ObjectId,
+    name: string,
+    score: number
+}
+
+interface IThought extends Document {
+    first: string,
+    last: string,
+    github: string,
+    reactions: Schema.Types.ObjectId[]
+}
+
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        name: {
+            type: String,
+            required: true,
+            maxlength: 50,
+            minlength: 4,
+            default: 'Unnamed reaction',
+        },
+        score: {
+            type: Number,
+            required: true,
+            default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+        },
+    },
+    {
+        timestamps: true,
+        _id: false
+    }
+);
+
+const studentSchema = new Schema<IThought>({
+    first: {
+        type: String,
+        required: true,
+        max_length: 50,
+    },
+    last: {
+        type: String,
+        required: true,
+        max_length: 50,
+    },
+    github: {
+        type: String,
+        required: true,
+        max_length: 50,
+    },
+    reactions: [reactionSchema],
+},
+    {
+        toJSON: {
+            getters: true,
+        },
+        timestamps: true
+    }
+);
+
+const Thought = model('Thought', studentSchema);
+
+export default Thought;
